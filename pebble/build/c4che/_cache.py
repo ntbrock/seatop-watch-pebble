a5de50b1a7a39e967ad6e0557369a7877fcc49d2
brockman <brockman@dis.local>
2016-02-09 19:13:36 -0500

    Removing build directory from source

--- pebble/build/c4che/_cache.py
+++
@@ -1,6 +0,0 @@
-BINDIR = '/usr/local/bin'
-DEFINES = ['RELEASE']
-LIBDIR = '/usr/local/lib'
-PEBBLE_SDK_COMMON = '/Users/brockman/Library/Application Support/Pebble SDK/SDKs/current/sdk-core/pebble/common'
-PREFIX = '/usr/local'
-TARGET_PLATFORMS = [u'aplite']
